cb594ab3e9d9ccbd6d9da842bb1622b259d6fc00
Daniel <danne_1998@hotmail.se>
2021-02-12 13:06:24 +0100

    finished 8queens problem and report

--- lab2/task4/queens.cpp
+++ lab2/task4/queens.cpp
@@ -5,19 +5,19 @@
 #include<vector>
 #include<omp.h>
 
-#define N 8
 #define MAX_THREADS 8
 
 bool print = false;
 bool data = false;
 int thread_tasks[MAX_THREADS];
 int solutions[MAX_THREADS];
+int n = 8;
 
 void print_solution(std::vector<int> qp){
     std::cout << "[";
-    for(int i = 0; i < N; i++){
+    for(int i = 0; i < n; i++){
         std::cout << qp[i];
-        if(i < N-1)
+        if(i < n-1)
             std::cout << ", ";
     }
     std::cout << "]\n";
@@ -26,9 +26,9 @@ void print_solution(std::vector<int> qp){
 bool check_solution(std::vector<int> qp){
     thread_tasks[omp_get_thread_num()]++;
     int a,b;
-    for(int i = 0; i < N; i++){
+    for(int i = 0; i < n; i++){
         a = qp[i];
-        for(int j = i + 1; j < N; j++){
+        for(int j = i + 1; j < n; j++){
             b = qp[j];
             if(a == b || a == b + (j - i) || a == b - (j - i))
                 return false;
@@ -38,52 +38,52 @@ bool check_solution(std::vector<int> qp){
 }
 
 void find_solutions(int k, std::vector<int> qp){
-    std::vector<int> qp_copy (N);
-    qp_copy = qp;
-    if(k == N){
-        #pragma omp task firstprivate(qp_copy)
-        {
-            if(check_solution(qp_copy)){
-                if(print){
-                    #pragma omp critical
-                    print_solution(qp_copy);
-                }
-                solutions[omp_get_thread_num()]++;
+    if(k == n){
+        if(check_solution(qp)){
+            if(print){
+                #pragma omp critical
+                print_solution(qp);
             }
+            solutions[omp_get_thread_num()]++;
         }
     }
-        
     else{
-        for(int i = 0; i < N; i++){
+        for(int i = 0; i < n; i++){
             qp[k] = i;
             find_solutions(k+1, qp);
         }
     }
 }
 
-int main(int argc, char const *argv[]){
-    double start, finish;
-    std::vector<int> qp (N);
-    if(argc == 3){
-        if(argv[2][0] == '1')
-            print = true;
-        else if(argv[2][0] == '2')
-            data = true;
+void parallel_solve(){
+    for(int i = 0; i < n; i++){
+        #pragma omp task
+        {
+            std::vector<int> qp (n);
+            qp[0] = i;
+            find_solutions(1, qp);
+        }
     }
-        
-    omp_set_num_threads(atoi(argv[1]));
+}
+
+double run(){
+    double start, finish;
 
     start = omp_get_wtime();
     #pragma omp parallel
     {
         #pragma omp single nowait 
         {
-            find_solutions(0, qp);
+            parallel_solve();
         }
     }
     #pragma omp taskwait
     finish = omp_get_wtime();
 
+    return finish - start;
+}
+
+void print_result(double result){
     if(data == false){
         int solution_count = 0;
         int sum = 0;
@@ -94,10 +94,29 @@ int main(int argc, char const *argv[]){
         }
         std::cout << "total tasks = " << sum << std::endl;
         std::cout << "solution count = " << solution_count << std::endl;
-        std::cout << "execution time = " << finish - start << std::endl;
+        std::cout << "execution time = " << result << std::endl;
     }
     else
-        std::cout << finish - start << std::endl;
+        std::cout << result << std::endl;
+}
+
+int main(int argc, char const *argv[]){
+    if(argc == 5){
+        if(argv[4][0] == '1')
+            print = true;
+        else if(argv[4][0] == '2')
+            data = true;
+    }
+    omp_set_num_threads(atoi(argv[1]));
+    n = atoi(argv[2]);
+    int iterations = atoi(argv[3]);
+    double result = 0;
+
+    for(int i = 0; i < iterations; i++)
+        result += run();
+    result = result/iterations;
+
+    print_result(result);
 
     return 0;
 }
